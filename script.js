37c34a5dff76f7cf1e897d0c9559faa8e8389476
Stino89 <ftr1989@gmail.com>
2024-02-11 10:02:44 -0800

    updated index

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById(`canvas1');
+const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
@@ -7,7 +7,7 @@ const playerImage = new Image();
 playerImage.src = 'shadow_dog.png';
 const spriteWidth = 575;
 const spriteHeight = 523;
-
+let playerState = 'run';
 
 let gameFrame = 0;
 const staggerFrames = 5;
@@ -22,8 +22,8 @@ const animationStates = [
         frames: 7,
     },
     {
-        name 'fall',
-    frames: 9,
+        name: 'fall',
+    frames: 7,
     },
     {
         name: 'run',
@@ -69,9 +69,9 @@ console.log(animationStates);
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations["jump"].loc.lenght;
+    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations[playerState].loc.lenght;
    let frameX = spriteWidth * position;
-    let frameY = spriteAnimations["jump"].loc[position].y;
+    let frameY = spriteAnimations[playerState].loc[position].y;
 
     ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0,  spriteWidth, spriteHeight);
    
